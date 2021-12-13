4afe81b91575f1ce7c12dc146defe2b471edf98c
MiBuena <athina.palada@gmail.com>
2021-12-13 20:52:01 +0200

    Revert "Comment out tests"
    This reverts commit d3b7a460330a7a0d73c6592b41e5830a11c92277.

--- ListGenerator.Web.UnitTests/ReplenishmentDataServiceTests/BaseReplenishmentDataServiceTests.cs
+++ ListGenerator.Web.UnitTests/ReplenishmentDataServiceTests/BaseReplenishmentDataServiceTests.cs
@@ -1,28 +1,28 @@
-﻿//using AutoMapper;
-//using ListGenerator.Data.Entities;
-//using ListGenerator.Server.Interfaces;
-//using ListGenerator.Server.Services;
-//using Moq;
-//using NUnit.Framework;
+﻿using AutoMapper;
+using ListGenerator.Data.Entities;
+using ListGenerator.Server.Interfaces;
+using ListGenerator.Server.Services;
+using Moq;
+using NUnit.Framework;
 
-//namespace ListGenerator.Web.UnitTests.ReplenishmentDataServiceTests
-//{
-//    public class BaseReplenishmentDataServiceTests : BaseItemsTests
-//    {
-//        [SetUp]
-//        protected virtual void Init()
-//        {
-//            ItemsRepositoryMock = new Mock<IRepository<Item>>(MockBehavior.Strict);
-//            PurchaseRepositoryMock = new Mock<IRepository<Purchase>>(MockBehavior.Strict);
-//            MapperMock = new Mock<IMapper>(MockBehavior.Strict);
-//            ReplenishmentItemBuilderMock = new Mock<IReplenishmentItemBuilder>(MockBehavior.Strict);
-//            ReplenishmentDataService = new ReplenishmentDataService(ItemsRepositoryMock.Object, MapperMock.Object, PurchaseRepositoryMock.Object, ReplenishmentItemBuilderMock.Object);
-//        }
+namespace ListGenerator.Web.UnitTests.ReplenishmentDataServiceTests
+{
+    public class BaseReplenishmentDataServiceTests : BaseItemsTests
+    {
+        [SetUp]
+        protected virtual void Init()
+        {
+            ItemsRepositoryMock = new Mock<IRepository<Item>>(MockBehavior.Strict);
+            PurchaseRepositoryMock = new Mock<IRepository<Purchase>>(MockBehavior.Strict);
+            MapperMock = new Mock<IMapper>(MockBehavior.Strict);
+            ReplenishmentItemBuilderMock = new Mock<IReplenishmentItemBuilder>(MockBehavior.Strict);
+            ReplenishmentDataService = new ReplenishmentDataService(ItemsRepositoryMock.Object, MapperMock.Object, PurchaseRepositoryMock.Object, ReplenishmentItemBuilderMock.Object);
+        }
 
-//        protected Mock<IRepository<Item>> ItemsRepositoryMock { get; private set; }
-//        protected Mock<IRepository<Purchase>> PurchaseRepositoryMock { get; private set; }
-//        protected Mock<IMapper> MapperMock { get; private set; }
-//        protected Mock<IReplenishmentItemBuilder> ReplenishmentItemBuilderMock { get; private set; }
-//        protected IReplenishmentDataService ReplenishmentDataService { get; private set; }
-//    }
-//}
+        protected Mock<IRepository<Item>> ItemsRepositoryMock { get; private set; }
+        protected Mock<IRepository<Purchase>> PurchaseRepositoryMock { get; private set; }
+        protected Mock<IMapper> MapperMock { get; private set; }
+        protected Mock<IReplenishmentItemBuilder> ReplenishmentItemBuilderMock { get; private set; }
+        protected IReplenishmentDataService ReplenishmentDataService { get; private set; }
+    }
+}
--- ListGenerator.Web.UnitTests/ReplenishmentDataServiceTests/GetShoppingListTests.cs
+++ ListGenerator.Web.UnitTests/ReplenishmentDataServiceTests/GetShoppingListTests.cs
@@ -1,110 +1,110 @@
-﻿//using FluentAssertions;
-//using FluentAssertions.Common;
-//using ListGenerator.Data.Entities;
-//using ListGenerator.Web.UnitTests.Helpers;
-//using Moq;
-//using NUnit.Framework;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
+﻿using FluentAssertions;
+using FluentAssertions.Common;
+using ListGenerator.Data.Entities;
+using ListGenerator.Web.UnitTests.Helpers;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-//namespace ListGenerator.Web.UnitTests.ReplenishmentDataServiceTests
-//{
-//    public class GetShoppingListTests : BaseReplenishmentDataServiceTests
-//    {
-//        [SetUp]
-//        protected override void Init()
-//        {
-//            base.Init();
-//        }
+namespace ListGenerator.Web.UnitTests.ReplenishmentDataServiceTests
+{
+    public class GetShoppingListTests : BaseReplenishmentDataServiceTests
+    {
+        [SetUp]
+        protected override void Init()
+        {
+            base.Init();
+        }
 
-//        //[Test]
-//        //public void Should_ReturnSuccessResponse_When_ValidInputparameters()
-//        //{
-//        //    //Arrange
-//        //    var allItems = BuildDifferentDatesItemsCollection();
-//        //    ItemsRepositoryMock.Setup(x => x.All()).Returns(allItems);
+        //[Test]
+        //public void Should_ReturnSuccessResponse_When_ValidInputparameters()
+        //{
+        //    //Arrange
+        //    var allItems = BuildDifferentDatesItemsCollection();
+        //    ItemsRepositoryMock.Setup(x => x.All()).Returns(allItems);
 
-//        //    var filteredItems = BuildDifferentDatesItemsCollectionFiltered();
-//        //    var filteredDtos = BuildDifferentDatesItemsDtosCollectionFiltered();
+        //    var filteredItems = BuildDifferentDatesItemsCollectionFiltered();
+        //    var filteredDtos = BuildDifferentDatesItemsDtosCollectionFiltered();
 
-//        //    var allDtos = BuildDifferentDatesItemsDtosCollection();
-//        //    ReplenishmentItemBuilderMock
-//        //        .Setup(x => x.BuildReplenishmentItemsDtos(
-//        //            It.Is<DateTime>(x => x.IsSameOrEqualTo(new DateTime(2020, 10, 4))),
-//        //            It.Is<DateTime>(x => x.IsSameOrEqualTo(new DateTime(2020, 10, 11))),
-//        //            It.Is<IEnumerable<Item>>(x => ItemsTestHelper.HaveTheSameElements(filteredItems, x))))
-//        //        .Returns(filteredDtos);
+        //    var allDtos = BuildDifferentDatesItemsDtosCollection();
+        //    ReplenishmentItemBuilderMock
+        //        .Setup(x => x.BuildReplenishmentItemsDtos(
+        //            It.Is<DateTime>(x => x.IsSameOrEqualTo(new DateTime(2020, 10, 4))),
+        //            It.Is<DateTime>(x => x.IsSameOrEqualTo(new DateTime(2020, 10, 11))),
+        //            It.Is<IEnumerable<Item>>(x => ItemsTestHelper.HaveTheSameElements(filteredItems, x))))
+        //        .Returns(filteredDtos);
 
-//        //    //Act
-//        //    var response = ReplenishmentDataService.GetShoppingList("04-10-2020", "11-10-2020", "ab70793b-cec8-4eba-99f3-cbad0b1649d0");
+        //    //Act
+        //    var response = ReplenishmentDataService.GetShoppingList("04-10-2020", "11-10-2020", "ab70793b-cec8-4eba-99f3-cbad0b1649d0");
 
-//        //    //Assert
-//        //    AssertHelper.AssertAll(
-//        //        () => response.IsSuccess.Should().BeTrue(),
-//        //        () => response.ErrorMessage.Should().BeNull()
-//        //        );
-//        //}
-
-
-//        //[Test]
-//        //public void Should_ReturnResponseWithThreeItems_When_ThreeItemsWithinShoppingListRange()
-//        //{
-//        //    //Arrange
-//        //    var allItems = BuildDifferentDatesItemsCollection();
-//        //    ItemsRepositoryMock.Setup(x => x.All()).Returns(allItems);
-
-//        //    var filteredItems = BuildDifferentDatesItemsCollectionFiltered();
-//        //    var filteredDtos = BuildDifferentDatesItemsDtosCollectionFiltered();
-
-//        //    var allDtos = BuildDifferentDatesItemsDtosCollection();
-//        //    ReplenishmentItemBuilderMock
-//        //        .Setup(x => x.BuildReplenishmentItemsDtos(
-//        //            It.Is<DateTime>(x => x.IsSameOrEqualTo(new DateTime(2020, 10, 4))),
-//        //            It.Is<DateTime>(x => x.IsSameOrEqualTo(new DateTime(2020, 10, 11))),
-//        //            It.Is<IEnumerable<Item>>(x => ItemsTestHelper.HaveTheSameElements(filteredItems, x))))
-//        //        .Returns(filteredDtos);
-
-//        //    //Act
-//        //    var response = ReplenishmentDataService.GetShoppingList("04-10-2020", "11-10-2020", "ab70793b-cec8-4eba-99f3-cbad0b1649d0");
-
-//        //    //Assert
-//        //    response.Data.Count().Should().Be(3);
-//        //}
+        //    //Assert
+        //    AssertHelper.AssertAll(
+        //        () => response.IsSuccess.Should().BeTrue(),
+        //        () => response.ErrorMessage.Should().BeNull()
+        //        );
+        //}
 
 
-//        //[Test]
-//        //public void Should_ReturnResponseWithCorrectFirstItem_When_ThreeItemsWithinShoppingListRange()
-//        //{
-//        //    //Arrange
-//        //    var allItems = BuildDifferentDatesItemsCollection();
-//        //    ItemsRepositoryMock.Setup(x => x.All()).Returns(allItems);
+        //[Test]
+        //public void Should_ReturnResponseWithThreeItems_When_ThreeItemsWithinShoppingListRange()
+        //{
+        //    //Arrange
+        //    var allItems = BuildDifferentDatesItemsCollection();
+        //    ItemsRepositoryMock.Setup(x => x.All()).Returns(allItems);
 
-//        //    var filteredItems = BuildDifferentDatesItemsCollectionFiltered();
-//        //    var filteredDtos = BuildDifferentDatesItemsDtosCollectionFiltered();
+        //    var filteredItems = BuildDifferentDatesItemsCollectionFiltered();
+        //    var filteredDtos = BuildDifferentDatesItemsDtosCollectionFiltered();
 
-//        //    var allDtos = BuildDifferentDatesItemsDtosCollection();
-//        //    ReplenishmentItemBuilderMock
-//        //        .Setup(x => x.BuildReplenishmentItemsDtos(
-//        //            It.Is<DateTime>(x => x.IsSameOrEqualTo(new DateTime(2020, 10, 4))),
-//        //            It.Is<DateTime>(x => x.IsSameOrEqualTo(new DateTime(2020, 10, 11))),
-//        //            It.Is<IEnumerable<Item>>(x => ItemsTestHelper.HaveTheSameElements(filteredItems, x))))
-//        //        .Returns(filteredDtos);
+        //    var allDtos = BuildDifferentDatesItemsDtosCollection();
+        //    ReplenishmentItemBuilderMock
+        //        .Setup(x => x.BuildReplenishmentItemsDtos(
+        //            It.Is<DateTime>(x => x.IsSameOrEqualTo(new DateTime(2020, 10, 4))),
+        //            It.Is<DateTime>(x => x.IsSameOrEqualTo(new DateTime(2020, 10, 11))),
+        //            It.Is<IEnumerable<Item>>(x => ItemsTestHelper.HaveTheSameElements(filteredItems, x))))
+        //        .Returns(filteredDtos);
 
-//        //    //Act
-//        //    var response = ReplenishmentDataService.GetShoppingList("04-10-2020", "11-10-2020", "ab70793b-cec8-4eba-99f3-cbad0b1649d0");
+        //    //Act
+        //    var response = ReplenishmentDataService.GetShoppingList("04-10-2020", "11-10-2020", "ab70793b-cec8-4eba-99f3-cbad0b1649d0");
 
-//        //    //Assert
-//        //    AssertHelper.AssertAll(
-//        //        () => response.Data.First().Id.Should().Be(1),
-//        //        () => response.Data.First().Name.Should().Be("Popcorn"),
-//        //        () => response.Data.First().NextReplenishmentDate.Should().BeSameDateAs(new DateTime(2020, 10, 02)),
-//        //        () => response.Data.First().Quantity.Should().Be(3),
-//        //        () => response.Data.First().ReplenishmentDate.Should().BeSameDateAs(new DateTime(2020, 10, 01)),
-//        //        () => response.Data.First().ItemNeedsReplenishmentUrgently.Should().BeTrue()
-//        //        );
-//        //}
+        //    //Assert
+        //    response.Data.Count().Should().Be(3);
+        //}
 
-//    }
-//}
+
+        //[Test]
+        //public void Should_ReturnResponseWithCorrectFirstItem_When_ThreeItemsWithinShoppingListRange()
+        //{
+        //    //Arrange
+        //    var allItems = BuildDifferentDatesItemsCollection();
+        //    ItemsRepositoryMock.Setup(x => x.All()).Returns(allItems);
+
+        //    var filteredItems = BuildDifferentDatesItemsCollectionFiltered();
+        //    var filteredDtos = BuildDifferentDatesItemsDtosCollectionFiltered();
+
+        //    var allDtos = BuildDifferentDatesItemsDtosCollection();
+        //    ReplenishmentItemBuilderMock
+        //        .Setup(x => x.BuildReplenishmentItemsDtos(
+        //            It.Is<DateTime>(x => x.IsSameOrEqualTo(new DateTime(2020, 10, 4))),
+        //            It.Is<DateTime>(x => x.IsSameOrEqualTo(new DateTime(2020, 10, 11))),
+        //            It.Is<IEnumerable<Item>>(x => ItemsTestHelper.HaveTheSameElements(filteredItems, x))))
+        //        .Returns(filteredDtos);
+
+        //    //Act
+        //    var response = ReplenishmentDataService.GetShoppingList("04-10-2020", "11-10-2020", "ab70793b-cec8-4eba-99f3-cbad0b1649d0");
+
+        //    //Assert
+        //    AssertHelper.AssertAll(
+        //        () => response.Data.First().Id.Should().Be(1),
+        //        () => response.Data.First().Name.Should().Be("Popcorn"),
+        //        () => response.Data.First().NextReplenishmentDate.Should().BeSameDateAs(new DateTime(2020, 10, 02)),
+        //        () => response.Data.First().Quantity.Should().Be(3),
+        //        () => response.Data.First().ReplenishmentDate.Should().BeSameDateAs(new DateTime(2020, 10, 01)),
+        //        () => response.Data.First().ItemNeedsReplenishmentUrgently.Should().BeTrue()
+        //        );
+        //}
+
+    }
+}
