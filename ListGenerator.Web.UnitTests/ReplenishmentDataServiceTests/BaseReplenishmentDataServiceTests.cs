09994591a5f3eb1f82eb053c1e090b6a514ac02d
MiBuena <athina.palada@gmail.com>
2021-12-14 19:33:38 +0200

    Fix BaseReplenishmentDataServiceTests

--- ListGenerator.Web.UnitTests/ReplenishmentDataServiceTests/BaseReplenishmentDataServiceTests.cs
+++ ListGenerator.Web.UnitTests/ReplenishmentDataServiceTests/BaseReplenishmentDataServiceTests.cs
@@ -1,28 +1,34 @@
-﻿//using AutoMapper;
-//using ListGenerator.Data.Entities;
-//using ListGenerator.Server.Interfaces;
-//using ListGenerator.Server.Services;
-//using Moq;
-//using NUnit.Framework;
+﻿using AutoMapper;
+using ListGeneration.Data.Interfaces;
+using ListGenerator.Data.Entities;
+using ListGenerator.Server.Interfaces;
+using ListGenerator.Server.Services;
+using Moq;
+using NUnit.Framework;
 
-//namespace ListGenerator.Web.UnitTests.ReplenishmentDataServiceTests
-//{
-//    public class BaseReplenishmentDataServiceTests : BaseItemsTests
-//    {
-//        [SetUp]
-//        protected virtual void Init()
-//        {
-//            ItemsRepositoryMock = new Mock<IRepository<Item>>(MockBehavior.Strict);
-//            PurchaseRepositoryMock = new Mock<IRepository<Purchase>>(MockBehavior.Strict);
-//            MapperMock = new Mock<IMapper>(MockBehavior.Strict);
-//            ReplenishmentItemBuilderMock = new Mock<IReplenishmentItemBuilder>(MockBehavior.Strict);
-//            ReplenishmentDataService = new ReplenishmentDataService(ItemsRepositoryMock.Object, MapperMock.Object, PurchaseRepositoryMock.Object, ReplenishmentItemBuilderMock.Object);
-//        }
+namespace ListGenerator.Web.UnitTests.ReplenishmentDataServiceTests
+{
+    public class BaseReplenishmentDataServiceTests : BaseItemsTests
+    {
+        [SetUp]
+        protected virtual void Init()
+        {
+            ItemsRepositoryMock = new Mock<IItemsRepository>(MockBehavior.Strict);
+            PurchaseRepositoryMock = new Mock<IAsyncRepository<Purchase>>(MockBehavior.Strict);
+            UnitOfWorkMock = new Mock<IUnitOfWork>(MockBehavior.Strict);
+            UnitOfWorkMock.Setup(x => x.ItemsRepository).Returns(ItemsRepositoryMock.Object);
+            UnitOfWorkMock.Setup(x => x.PurchasesRepository).Returns(PurchaseRepositoryMock.Object);
 
-//        protected Mock<IRepository<Item>> ItemsRepositoryMock { get; private set; }
-//        protected Mock<IRepository<Purchase>> PurchaseRepositoryMock { get; private set; }
-//        protected Mock<IMapper> MapperMock { get; private set; }
-//        protected Mock<IReplenishmentItemBuilder> ReplenishmentItemBuilderMock { get; private set; }
-//        protected IReplenishmentDataService ReplenishmentDataService { get; private set; }
-//    }
-//}
+            MapperMock = new Mock<IMapper>(MockBehavior.Strict);
+            ReplenishmentItemBuilderMock = new Mock<IReplenishmentItemBuilder>(MockBehavior.Strict);
+            ReplenishmentDataService = new ReplenishmentDataService(UnitOfWorkMock.Object, MapperMock.Object, ReplenishmentItemBuilderMock.Object);
+        }
+
+        protected Mock<IUnitOfWork> UnitOfWorkMock { get; private set; }
+        protected Mock<IItemsRepository> ItemsRepositoryMock { get; private set; }
+        protected Mock<IAsyncRepository<Purchase>> PurchaseRepositoryMock { get; private set; }
+        protected Mock<IMapper> MapperMock { get; private set; }
+        protected Mock<IReplenishmentItemBuilder> ReplenishmentItemBuilderMock { get; private set; }
+        protected IReplenishmentDataService ReplenishmentDataService { get; private set; }
+    }
+}
